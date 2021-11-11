bfa8270d56f8e48ef85259a8b8c2e45db969f32b
Giorgi Khmaladze <gkhmalad@purdue.edu>
2021-11-11 01:47:20 -0500

    crosscheck between different OS for continuity

--- test/main.py
+++ test/main.py
@@ -1,10 +1,10 @@
 import hashlib
 
 h = hashlib.sha3_256(b"Nobody inspects the spammish repetition")
-m = hashlib.sha3_256(b"Nobody inspects the spammish repetition")
+
 
 print(h.hexdigest())
-print(m.hexdigest())
+
 
 
 print('Hello')
