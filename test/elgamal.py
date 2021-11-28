e84ea495a3f685345b409975220a23f93f7a25d6
Giorgi Khmaladze <gkhmalad@purdue.edu>
2021-11-28 00:54:58 -0500

    proper implementation of enc and dec algos of elgamal

--- test/elgamal.py
+++ test/elgamal.py
@@ -1,92 +1,28 @@
-# Python program to illustrate ElGamal encryption
-# Copied from Geeks4Geeks
+# Encryption algorithm
+def enc(msg, modulo, receiverPublicExponent, publicBase, sendersPublicExponent):
+    
+    # ciphertext container
+    c = []
 
-import random
-from math import pow
+    # calculate shared key
+    sharedKey = pow(receiverPublicExponent, sendersPublicExponent, modulo)
 
-#Random integer from 2 to 10
-a = random.randint(2, 10)
+    # copy msg characters into ciphertext container
+    for i in range(0, len(msg)):
+        c.append(msg[i])
 
-#Greatest common divisor
-def gcd(a, b):
-	if a < b:
-		return gcd(b, a)
-	elif a % b == 0:
-		return b
-	else:
-		return gcd(b, a % b)
+    # multiply each characters int representative by the shared key
+    for i in range(0, len(c)):
+        c[i] = sharedKey * ord(c[i])
 
-# Generating large random numbers
-def gen_key(q):
+    return c[i]
 
-	key = random.randint(pow(10, 20), q)
+# Decryption algorithm
+def dec(cipher, modulo, sendersPublicExponent, sharedKey):
 
-    # Ensuring prime
-	while gcd(q, key) != 1:
-		key = random.randint(pow(10, 20), q)
+    plaintext = []
 
-	return key
-
-# Modular exponentiation
-def power(a, b, c):
-	x = 1
-	y = a
-
-	while b > 0:
-		if b % 2 != 0:
-			x = (x * y) % c
-		y = (y * y) % c
-		b = int(b / 2)
-
-	return x % c
-
-# Asymmetric encryption
-def encrypt(msg, q, h, g):
-
-	en_msg = []
-
-	k = gen_key(q)# Private key for sender
-	s = power(h, k, q)
-	p = power(g, k, q)
-	
-	for i in range(0, len(msg)):
-		en_msg.append(msg[i])
-
-	print("g^k used : ", p)
-	print("g^ak used : ", s)
-	for i in range(0, len(en_msg)):
-		en_msg[i] = s * ord(en_msg[i])
-
-	return en_msg, p
-
-def decrypt(en_msg, p, key, q):
-
-	dr_msg = []
-	h = power(p, key, q)
-	for i in range(0, len(en_msg)):
-		dr_msg.append(chr(int(en_msg[i]/h)))
-		
-	return dr_msg
-
-# Driver code
-def main():
-
-	msg = 'encryption'
-	print("Original Message :", msg)
-
-	q = random.randint(pow(10, 20), pow(10, 50))
-	g = random.randint(2, q)
-
-	key = gen_key(q)# Private key for receiver
-	h = power(g, key, q)
-	print("g used : ", g)
-	print("g^a used : ", h)
-
-	en_msg, p = encrypt(msg, q, h, g)
-	dr_msg = decrypt(en_msg, p, key, q)
-	dmsg = ''.join(dr_msg)
-	print("Decrypted Message :", dmsg)
+    for i in range(0, len(cipher)):
+        plaintext.append(chr(int(cipher[i]/sharedKey)))
 
 
-if __name__ == '__main__':
-	main()
