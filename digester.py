959edfd4ab19d0a10c59b74eb16497101fcdc2d8
Giorgi Khmaladze <gkhmalad@purdue.edu>
2021-11-29 09:25:12 -0500

    object orient digester

--- digester.py
+++ digester.py
@@ -1,8 +1,11 @@
 import hashlib
 
-# rb mode reads in binary, omits \n and such
-with open("passwords.txt", 'rb') as file:
+def file_digest(fpath):
+
+    # rb mode reads in binary, omits \n and such
+    with open(fpath, 'rb') as file:
         data = file.read()
 
-h =  hashlib.sha3_512(data)
-print(h.hexdigest())
+    h =  hashlib.sha3_512(data)
+
+    return h.hexdigest()
