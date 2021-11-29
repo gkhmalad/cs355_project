c66cea44b0f50683f6559e792d462b0e26d7f59b
Giorgi Khmaladze <gkhmalad@purdue.edu>
2021-11-29 09:19:37 -0500

    creating code to read file contents and create digest

---
+++ digester.py
@@ -0,0 +1,7 @@
+import hashlib
+
+# h = hashlib.sha3_512(b"Nobody inspects the spammish repetition")
+
+# print(h.hexdigest())
+
+# print('Hello')
--- test/main.py
+++ test/main.py
@@ -2,9 +2,6 @@ import hashlib
 
 h = hashlib.sha3_256(b"Nobody inspects the spammish repetition")
 
-
 print(h.hexdigest())
-
-
 
 print('Hello')
